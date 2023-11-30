47636ad421136c03d157c96448440b55d23c097e
Abhinav Jain <jainabhinav0860@gmail.com>
2023-11-30 17:22:04 +0530

    frontend for new collections

--- src/components/category.js
+++ src/components/category.js
@@ -2,19 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { Text, View, Image, StatusBar, Button, TextInput, ScrollView, TouchableOpacity, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
+import Animated, { useSharedValue, withSpring, FadeInDown } from 'react-native-reanimated';
 
 import { collectionData } from '../constants';
 
 function Category() {
   return (
-    <View style={{ paddingTop: hp(1.5), paddingBottom: hp(0.5) }}>
+    <View
+      style={{ paddingTop: hp(1.5), paddingBottom: hp(0.5) }}>
       <ScrollView
         horizontal
         className="space-x-7"
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{ paddingHorizontal: 15 }}
       >
+        {/* <CategoryCard sourceImage={require('../assets/images/model-men.png')} title='Men' index={1} />
+        <CategoryCard sourceImage={require('../assets/images/model-women.png')} title='Women' index={1} />
+        <CategoryCard sourceImage={require('../assets/images/model-kids.png')} title='Kids' index={1} />
+        <CategoryCard sourceImage={require('../assets/images/winter-wear.png')} title='Winter Wear' index={1} />
+        <CategoryCard sourceImage={require('../assets/images/footwear.png')} title='Footwear' index={1} /> */}
+
         <TouchableOpacity className="flex items-center space-y-1">
           <View className={"rounded-lg items-center"}>
             <Image source={require('../assets/images/model-men.png')} style={{ width: hp(8), height: hp(7.1) }} />
@@ -54,5 +61,16 @@ function Category() {
     </View>
   );
 }
+
+// const CategoryCard = ({ sourceImage, title, index }) => {
+//   return (
+//     <TouchableOpacity className="flex items-center" style={{ marginLeft: hp(1), marginRight: hp(1) }}>
+//       <View className={"rounded-lg items-center"}>
+//         <Image source={require('../assets/images/model-men.png')} style={{ width: hp(8), height: hp(7.1) }} />
+//         <Text className="text-base tracking-wider text-black" style={{ fontSize: hp(1.8) }}>Men</Text>
+//       </View>
+//     </TouchableOpacity>
+//   )
+// }
 
 export default Category;
--- src/components/collections.js
+++ src/components/collections.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, Image, Pressable } from 'react-native';
+import { Text, View, Image, Pressable, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Animated, { useSharedValue, withSpring, FadeInDown } from 'react-native-reanimated';
 import MasonryList from '@react-native-seoul/masonry-list';
 import { TagIcon } from 'react-native-heroicons/outline';
+import { HeartIcon } from 'react-native-heroicons/solid';
+import LinearGradient from 'react-native-linear-gradient';
 
 import { collectionData } from '../constants';
 
@@ -29,41 +31,61 @@ function Collections() {
   );
 }
 
-const CollectionCard = ({ item, index }) => (
-  <Animated.View entering={FadeInDown.delay(index * 100).duration(400).springify().damping(12)}>
-    <Pressable
-      className="flex flex-1 justify-center"
-      style={{ width: '100%', paddingLeft: hp(0.5), paddingRight: hp(0.5), marginBottom: hp(1.5) }}
-      onPress={() => console.log('Collection Pressed')}>
-      <Image style={{ width: '100%', height: hp(28), borderRadius: 10 }}
-        className="bg-black/5" source={require('../assets/images/Dummy-women.png')} />
-      <View className="flex">
-        <Text style={{ fontSize: hp(1.9) }}
-          className="font-bold text-neutral-950 tracking-wide">
-          Company name
-        </Text>
-        <Text style={{ fontSize: hp(1.6) }}
-          className="text-neutral-600 font-semibold tracking-wide">
-          {item.name.length > 20 ? item.name.slice(0, 20) + '...' : item.name}
-        </Text>
-        <View className="flex flex-row">
-          <Text style={{ fontSize: hp(1.8), paddingTop: hp(0.1) }}
-            className="text-neutral-500 font-light line-through">₹1,999
-          </Text >
-          <Text style={{ fontSize: hp(1.9), paddingTop: hp(0.1) }}
-            className="text-neutral-800 font-semibold tracking-wide ml-1">₹576
-          </Text>
-          <View className="flex flex-1 flex-row ml-1 bg-orange-300 items-center">
-            <TagIcon color="white" size={hp(2)} strokeWidth={2.5} />
-            <Text style={{ fontSize: hp(1.3), paddingTop: hp(0.1) }}
-              className="font-bold tracking-wide ml-1 text-white">67% OFF!
-            </Text>
-          </View>
-        </View>
+const CollectionCard = ({ item, index }) => {
+  const [heartIconColorState, setHeartIconColorState] = useState("white");
 
-      </View>
-    </Pressable>
-  </Animated.View>
-);
+  const handleHeartIconState = () => {
+    setHeartIconColorState((prevColor) => (prevColor === 'white' ? 'red' : 'white'));
+  }
+
+  return (
+    <Animated.View entering={FadeInDown.delay(index * 100).duration(400).springify().damping(12)}>
+      <Pressable
+        className="flex flex-1 justify-center"
+        style={{ width: '100%', paddingLeft: hp(0.5), paddingRight: hp(0.5), marginBottom: hp(1.5) }}
+        onPress={() => console.log('Collection Pressed')}>
+        {/* Image of Product */}
+        <View className="relative flex-1">
+          <Image style={{ width: '100%', height: hp(28), borderRadius: 10 }}
+            className="bg-black/5" source={require('../assets/images/Dummy-women.png')} />
+          <TouchableOpacity className='absolute'
+            style={{ top: hp(0.8), right: hp(1) }}
+            onPress={handleHeartIconState}>
+            <HeartIcon color={heartIconColorState} size={hp(3)} strokeWidth={2.5} />
+          </TouchableOpacity>
+        </View>
+        {/* Label Section  */}
+        <View className="flex">
+          <Text style={{ fontSize: hp(1.9) }}
+            className="font-bold text-neutral-950 tracking-wide">
+            Company name
+          </Text>
+          <Text style={{ fontSize: hp(1.6) }}
+            className="text-neutral-600 font-semibold tracking-wide">
+            {item.name.length > 20 ? item.name.slice(0, 20) + '...' : item.name}
+          </Text>
+          <View className="flex flex-row">
+            <Text style={{ fontSize: hp(1.8), paddingTop: hp(0.1) }}
+              className="text-neutral-500 font-light line-through">₹1,999
+            </Text >
+            <Text style={{ fontSize: hp(1.9), paddingTop: hp(0.1) }}
+              className="text-neutral-800 font-semibold tracking-wide ml-1">₹576
+            </Text>
+            <LinearGradient className="flex flex-1 flex-row ml-1 justify-center items-center rounded-lg"
+              colors={['#fb923c', '#fb7185']}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}>
+              <TagIcon color="white" size={hp(2)} strokeWidth={2.5} />
+              <Text style={{ fontSize: hp(1.3), paddingTop: hp(0.1) }}
+                className="font-bold tracking-wide ml-1 text-white">67% OFF!
+              </Text>
+            </LinearGradient>
+          </View>
+
+        </View>
+      </Pressable>
+    </Animated.View>
+  )
+}
 
 export default Collections;
